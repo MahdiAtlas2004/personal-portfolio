f626076093406ee7d5aff5f38c386271d4492b47
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-07-01 10:26:32 +0430

    banner basic structure added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import "./App.css";
 import NavBar from "./components/NavBar";
+import Banner from "./components/Banner";
+
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
 
 function NavBar() {
@@ -30,7 +34,7 @@ function NavBar() {
     <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
-          <img src={""} alt="logo" />
+          <img src={logo} alt="logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
@@ -67,9 +71,9 @@ function NavBar() {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#" alt=""></a>
-              <a href="#" alt=""></a>
-              <a href="#" alt=""></a>
+              <a href="#"><img src={navIcon1} alt=""/></a>
+              <a href="#"><img src={navIcon3} alt=""/></a>
+              <a href="#"><img src={navIcon2} alt=""/></a>
             </div>
             <button className="vvd" onClick={() => console.log("connect")}>
               Let's Connect
