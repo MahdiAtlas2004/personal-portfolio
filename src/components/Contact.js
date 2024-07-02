c19533b91ef2a2a66c8398f2c0139241495bc965
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-07-02 10:27:51 +0430

    server.js file added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,9 +21,28 @@ const Contact = () => {
     });
   };
 
-  const handleSubmit = () => {
-
-  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setButtonText("Sending...");
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json;charset=utf-8",
+      },
+      body: JSON.stringify(formDetails),
+    });
+    setButtonText("Send");
+    let result = response.json();
+    setFormDetails(formInitialDetails);
+    if (result.code === 200) {
+      setStatus({ success: true, message: "Message sent successfully" });
+    } else {
+      setStatus({
+        success: false,
+        message: "Something went wrong, please try again later.",
+      });
+    }
+  };
 
   return (
     <section className="contact" id="connect">
