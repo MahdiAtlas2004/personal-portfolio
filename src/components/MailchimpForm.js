b0f37f212f213c2c07b93b03c4999de4971a896f
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-07-03 05:50:27 +0430

    MailchimpForm and .env files added

---
+++ src/components/MailchimpForm.js
@@ -0,0 +1,16 @@
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+
+const MailchimpForm = () => {
+  const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
+
+  return (
+    <>
+      <MailchimpSubscribe
+        url={postUrl}
+        render={({ subscribe, status, message }) => <></>}
+      />
+    </>
+  );
+};
+
+export default MailchimpForm;
