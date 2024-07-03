fb0f7b9f2da9a4d2ac485c6e6ff47d2f072d661f
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-07-03 17:21:33 +0430

    CSS styles and Logos for all sections updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,29 @@
 import { Container, Row, Col } from "react-bootstrap";
-import MailchimpForm from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
+// import MailchimpForm from "./MailchimpForm";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <Container>
-        <Row className="align-items-center">
-          <MailchimpForm />
-          <Col sm={6}>
-            <img src={logo} alt="Logo" />
+        <Row className="align-items-center pt-4">
+          {/* <MailchimpForm /> */}
+          <Col sm={12} lg={6} className="text-white fs-1 fw-bold d-flex justify-content-lg-start justify-content-center">
+            <div className="my-logo">Mahdi Atlas</div>
           </Col>
-          <Col sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="navIcon1" /></a>
-                <a href="#"><img src={navIcon2} alt="navIcon2" /></a>
-                <a href="#"><img src={navIcon3} alt="navIcon3" /></a>
+          <Col sm={12} lg={6} className="text-center text-sm-end">
+            <div className="social-icon align-items-center d-flex justify-content-lg-end justify-content-center mt-sm-4">
+              <a href="#">
+                <img src={navIcon1} alt="navIcon1"/>
+              </a>
+              <a href="#">
+                <img src={navIcon2} alt="navIcon2"  className="github"/>
+              </a>
+              <a href="#">
+                <img src={navIcon3} alt="navIcon3" className="telegram"/>
+              </a>
             </div>
             <p>CopyRight 2024. All Right Reserved by Mahdi Atlas</p>
           </Col>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,9 @@ import { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
-
 
 function NavBar() {
   const [activeLink, setActiveLink] = useState("home");
@@ -33,8 +31,8 @@ function NavBar() {
   return (
     <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
-        <Navbar.Brand href="#home">
-          <img src={logo} alt="logo" />
+        <Navbar.Brand href="#home" className="text-white fs-1 fw-bold">
+          Atlas
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
@@ -70,10 +68,10 @@ function NavBar() {
             </Nav.Link>
           </Nav>
           <span className="navbar-text">
-            <div className="social-icon">
+            <div className="social-icon align-items-center d-flex">
               <a href="#"><img src={navIcon1} alt=""/></a>
-              <a href="#"><img src={navIcon3} alt=""/></a>
-              <a href="#"><img src={navIcon2} alt=""/></a>
+              <a href="#"><img src={navIcon2} alt="" className="github"/></a>
+              <a href="#"><img src={navIcon3} alt="" className="telegram"/></a>
             </div>
             <button className="vvd" onClick={() => console.log("connect")}>
               Let's Connect
