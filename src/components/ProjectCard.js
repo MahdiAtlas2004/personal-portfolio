e6b39d8d25ce17c866d0db0cb38fc0468100dc0b
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-07-14 00:15:49 +0430

    webUrl property added to the ProjectCard

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,17 @@
 import { Col } from "react-bootstrap";
 
-const ProjectCard = ({ title, description, imgUrl }) => {
+const ProjectCard = ({ title, description, imgUrl, webUrl = "#" }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} alt="img" />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
+      <a href={webUrl} rel="noopener noreferrer" target="_blank" style={{color: "white", textDecoration: "none", cursor: "pointer" }}>
+        <div className="proj-imgbx">
+          <img src={imgUrl} alt="img" />
+          <div className="proj-txtx">
+            <h4>{title}</h4>
+            <span>{description}</span>
+          </div>
         </div>
-      </div>
+      </a>
     </Col>
   );
 };
